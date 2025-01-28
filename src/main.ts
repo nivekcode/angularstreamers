9eb258cd803088b043c7b7a5adae01847c199baa
kreuzerk <kevin.kreuzer90@icloud.com>
2025-01-28 18:17:09 +0100

    feat: 🎸 call inject

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from "@vercel/analytics"
 
+inject();
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
